14b14db5b8a90bd177522a7c77e653be8e965fe6
Philip Ruehl <ruehl@hep.physik.uni-siegen.de>
2020-10-20 02:37:48 +0200

    Fixed Skybox

--- objects/skybox.cpp
+++ objects/skybox.cpp
@@ -111,16 +111,15 @@ void Skybox::createObject()
     std::vector<glm::vec2> texCoords;
     positions.clear();
     indices.clear();
-
-    //Cube front from top left clockwise: 3,6,7,2. Backside parallel points: 1,5,4,0
-    positions.push_back(glm::vec3(-1,-1,-1) *1.f); // 0 -> 0
-    positions.push_back(glm::vec3(-1, 1, -1)*1.f); // 1 -> 1
-    positions.push_back(glm::vec3(-1,-1, 1) *1.f); // 4 -> 2
-    positions.push_back(glm::vec3(-1, 1, 1) *1.f); // 5 -> 3
-    positions.push_back(glm::vec3( 1,-1,-1) *1.f); // 6 -> 4
-    positions.push_back(glm::vec3( 1, 1,-1) *1.f); // 7 -> 5
-    positions.push_back(glm::vec3( 1,-1, 1) *1.f); // 2 -> 6
-    positions.push_back(glm::vec3( 1, 1, 1) *1.f); // 3 -> 7
+    //Cube coordinates
+    positions.push_back(glm::vec3(-1,-1,-1) *1.f); // 0
+    positions.push_back(glm::vec3(-1,-1, 1) *1.f); // 1
+    positions.push_back(glm::vec3(-1, 1,-1) *1.f); // 2
+    positions.push_back(glm::vec3(-1, 1, 1) *1.f); // 3
+    positions.push_back(glm::vec3( 1,-1,-1) *1.f); // 4
+    positions.push_back(glm::vec3( 1,-1, 1) *1.f); // 5
+    positions.push_back(glm::vec3( 1, 1,-1) *1.f); // 6
+    positions.push_back(glm::vec3( 1, 1, 1) *1.f); // 7
 
     texCoordOptions.push_back(glm::vec2(0,0));
     texCoordOptions.push_back(glm::vec2(0,1));
@@ -128,136 +127,134 @@ void Skybox::createObject()
     texCoordOptions.push_back(glm::vec2(1,1));
 
     // indices (two triangles for each side of the cube)
-        // left
-        indices.push_back(0);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(1);
-        texCoords.push_back(texCoordOptions.at(1));
-        indices.push_back(2);
-        texCoords.push_back(texCoordOptions.at(2));
+    // left (x = -1)
+    indices.push_back(0);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(2);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(1);
+    texCoords.push_back(texCoordOptions.at(2));
 
-        indices.push_back(2);
-        texCoords.push_back(texCoordOptions.at(2));
-        indices.push_back(1);
-        texCoords.push_back(texCoordOptions.at(1));
-        indices.push_back(3);
-        texCoords.push_back(texCoordOptions.at(3));
+    indices.push_back(1);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(2);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(3);
+    texCoords.push_back(texCoordOptions.at(3));
 
-        // back
-        indices.push_back(0);
-        texCoords.push_back(glm::vec2(0,0));
-        indices.push_back(4);
-        texCoords.push_back(glm::vec2(1,0));
-        indices.push_back(1);
-        texCoords.push_back(glm::vec2(0,1));
+    // right (x = 1)
+    indices.push_back(4);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(5);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(6);
+    texCoords.push_back(texCoordOptions.at(2));
 
+    indices.push_back(5);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(7);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(6);
+    texCoords.push_back(texCoordOptions.at(3));
 
-        indices.push_back(1);
-        texCoords.push_back(texCoordOptions.at(1));
-        indices.push_back(4);
-        texCoords.push_back(texCoordOptions.at(2));
-        indices.push_back(5);
-        texCoords.push_back(texCoordOptions.at(3));
+    // bottom (y = -1)
+    indices.push_back(0);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(1);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(4);
+    texCoords.push_back(texCoordOptions.at(2));
 
-        // top
-        indices.push_back(1);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(5);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(3);
-        texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(4);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(1);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(5);
+    texCoords.push_back(texCoordOptions.at(3));
 
-        indices.push_back(5);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(6);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(3);
-        texCoords.push_back(texCoordOptions.at(0));
+    // top (y = 1)
+    indices.push_back(2);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(6);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(3);
+    texCoords.push_back(texCoordOptions.at(2));
 
-        // right
-        indices.push_back(7);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(6);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(4);
-        texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(3);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(6);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(7);
+    texCoords.push_back(texCoordOptions.at(3));
 
-        indices.push_back(6);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(5);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(4);
-        texCoords.push_back(texCoordOptions.at(0));
+    // back (z = -1)
+    indices.push_back(0);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(4);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(2);
+    texCoords.push_back(texCoordOptions.at(2));
 
-        // front
-        indices.push_back(2);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(3);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(7);
-        texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(4);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(6);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(2);
+    texCoords.push_back(texCoordOptions.at(3));
 
-        indices.push_back(3);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(6);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(7);
-        texCoords.push_back(texCoordOptions.at(0));
+    // front (z = 1)
+    indices.push_back(1);
+    texCoords.push_back(texCoordOptions.at(0));
+    indices.push_back(3);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(5);
+    texCoords.push_back(texCoordOptions.at(2));
 
-        // bottom
-        indices.push_back(4);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(0);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(7);
-        texCoords.push_back(texCoordOptions.at(0));
-
-        indices.push_back(0);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(2);
-        texCoords.push_back(texCoordOptions.at(0));
-        indices.push_back(7);
-        texCoords.push_back(texCoordOptions.at(0));
-
+    indices.push_back(3);
+    texCoords.push_back(texCoordOptions.at(2));
+    indices.push_back(7);
+    texCoords.push_back(texCoordOptions.at(1));
+    indices.push_back(5);
+    texCoords.push_back(texCoordOptions.at(3));
 
 
 
-        // Set up a vertex array object for the geometry
-        if(_vertexArrayObject == 0)
-            glGenVertexArrays(1, &_vertexArrayObject);
-        glBindVertexArray(_vertexArrayObject);
+    // Set up a vertex array object for the geometry
+    if(_vertexArrayObject == 0)
+        glGenVertexArrays(1, &_vertexArrayObject);
+    glBindVertexArray(_vertexArrayObject);
 
-        // fill vertex array object with data
-        GLuint position_buffer;
-        glGenBuffers(1, &position_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
-        glBufferData(GL_ARRAY_BUFFER, positions.size() * 3 * sizeof(float), positions.data(), GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glEnableVertexAttribArray(0);
+    // fill vertex array object with data
+    GLuint position_buffer;
+    glGenBuffers(1, &position_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, positions.size() * 3 * sizeof(float), positions.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
 
-        // Hint: the texture coordinates buffer is missing
+    // Hint: the texture coordinates buffer is missing
 
-        GLuint index_buffer;
-        glGenBuffers(1, &index_buffer);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+    GLuint index_buffer;
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
 
-        GLuint tex_buffer;
-        glGenBuffers(1,&tex_buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, tex_buffer);
-        glBufferData(GL_ARRAY_BUFFER, texCoords.size()*sizeof (glm::vec2),texCoords.data(),GL_STATIC_DRAW);
-        glVertexAttribPointer(1,2,GL_FLOAT,GL_TRUE,0,0);
-        glEnableVertexAttribArray(1);
+    GLuint tex_buffer;
+    glGenBuffers(1,&tex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, tex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, texCoords.size()*sizeof (glm::vec2),texCoords.data(),GL_STATIC_DRAW);
+    glVertexAttribPointer(1,2,GL_FLOAT,GL_TRUE,0,0);
+    glEnableVertexAttribArray(1);
 
-        // unbind vertex array object
-        glBindVertexArray(0);
-        // delete buffers (the data is stored in the vertex array object)
-        glDeleteBuffers(1, &position_buffer);
-        glDeleteBuffers(1, &index_buffer);
-        glDeleteBuffers(1,&tex_buffer);
+    // unbind vertex array object
+    glBindVertexArray(0);
+    // delete buffers (the data is stored in the vertex array object)
+    glDeleteBuffers(1, &position_buffer);
+    glDeleteBuffers(1, &index_buffer);
+    glDeleteBuffers(1,&tex_buffer);
 
-        // check for errors
-        VERIFY(CG::checkError());
+    // check for errors
+    VERIFY(CG::checkError());
 }
 
